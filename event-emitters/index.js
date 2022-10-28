c6656af3e4e622e7a9e888144755663e4606bf10
RadicaIEdward <edward.o.galindez@gmail.com>
2022-10-28 15:10:15 -0400

    Updated index.js to remove all listeners

--- event-emitters/index.js
+++ event-emitters/index.js
@@ -16,9 +16,33 @@ ticketManager.on('buy', (email, price, timestamp) => {
   databaseService.save(email, price, timestamp);
 });
 
+// Error handling
 ticketManager.on('error', (error) => {
   error(chalk.bgMagentaBright.underline(`Gracefully handling our error: ${error}`));
 });
 
+// Listener counts
 log(chalk.greenBright.bgGray(`We have ${ticketManager.listenerCount('buy')} listener(s) for the buy event`));
 log(chalk.cyan.bgGrey(`We have ${ticketManager.listenerCount('error')} listener(s) for the error event`));
+
+// Add new event listener
+const onBuy = () => {
+  log(chalk.greenBright.italic('I will be removed soon'));
+};
+
+ticketManager.on('buy', onBuy);
+
+log(chalk.bgMagenta.underline(`We added a new event listener bringing our total count for the buy event to: ${ticketManager.listenerCount('buy')}`));
+ticketManager.buy('test@email', 20);
+
+// Remove a single even listener
+ticketManager.off('buy', onBuy);
+
+log(chalk.blackBright.bgYellowBright.bold(`We now have: ${ticketManager.listenerCount('buy')} listener(s) for the buy event`));
+ticketManager.buy('test@email', 20);
+
+// Remove all listeners
+ticketManager.removeAllListeners('buy');
+log(chalk.bgRed.bold(`We have ${ticketManager.listenerCount('buy')} listeners for the buy event`));
+ticketManager.buy('test@email', 20);
+log(chalk.bgRed.bold.underline('The last ticket was bought'));
