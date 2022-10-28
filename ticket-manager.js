4d0dec98d5560c6018403421c580eb726634051d
RadicaIEdward <edward.o.galindez@gmail.com>
2022-10-28 12:52:24 -0400

    Create buy method

--- ticket-manager.js
+++ ticket-manager.js
@@ -1,21 +1,27 @@
 import chalk from 'chalk';
 import { EventEmitter } from 'node:events';
 
+const { log } = console;
+
 class TicketManager extends EventEmitter {
   constructor(supply) {
     super();
     this.supply = supply;
   }
+
+  buy(email, price) {
+    this.supply--;
+    this.emit('buy', email, price, Date.now());
+  }
 }
-const { log } = console;
 
 // Basic colors
-log(chalk.red('Hey this is red!'));
-log(chalk.magenta('Hey this is magenta!'));
-log(chalk.yellow('Hey this is yellow!'));
+// log(chalk.red('Hey this is red!'));
+// log(chalk.magenta('Hey this is magenta!'));
+// log(chalk.yellow('Hey this is yellow!'));
 
 // Background colors
-log(chalk.bgRed('Red background'));
+// log(chalk.bgRed('Red background'));
 
-// Styling & decoration
-log(chalk.bold('Bold!'));
+/// Styling & decoration
+// log(chalk.bold('Bold!'));
